4f8a497d7c7b5732e2e0dc96cfbad67aadb14a8a
guegit <ctbcban@gmail.com>
2023-01-07 23:43:16 +0900

    update colors

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -1,40 +1,43 @@
-import React from "react"
-import { graphql } from "gatsby"
-import styled from "@emotion/styled"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import React from "react";
+import { graphql } from "gatsby";
+import styled from "@emotion/styled";
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const Content = styled.div`
   margin: 0 auto;
   max-width: 860px;
   padding: 1.45rem 1.0875rem;
-`
+`;
 
 const MarkedHeader = styled.h1`
   display: inline;
   border-radius: 1em 0 1em 0;
+  color: ${(props) => props.theme.colors.error};
   background-image: linear-gradient(
     -100deg,
-    rgba(255, 250, 150, 0.15),
-    rgba(255, 250, 150, 0.8) 100%,
-    rgba(255, 250, 150, 0.25)
+    ${(props) => `hsl(${props.theme.colors.errorContainerHsl} / 0.15)`},
+    ${(props) => `hsl(${props.theme.colors.errorContainerHsl} / 0.8) 100%`},
+    ${(props) => `hsl(${props.theme.colors.errorContainerHsl} / 0.25)`}
   );
-`
+`;
 
 const HeaderDate = styled.h3`
   margin-top: 10px;
   color: #606060;
-`
+`;
 
 // STYLE THE TAGS INSIDE THE MARKDOWN HERE
 const MarkdownContent = styled.div`
+  color: ${(props) => props.theme.colors.onBackground};
   a {
     text-decoration: none;
     position: relative;
+    color: ${(props) => props.theme.colors.surfaceTint};
 
     background-image: linear-gradient(
-      rgba(255, 250, 150, 0.8),
-      rgba(255, 250, 150, 0.8)
+      ${(props) => props.theme.colors.outlineVariant},
+      ${(props) => props.theme.colors.outlineVariant}
     );
     background-repeat: no-repeat;
     background-size: 100% 0.2em;
@@ -48,10 +51,10 @@ const MarkdownContent = styled.div`
   a > code:hover {
     text-decoration: underline;
   }
-`
+`;
 
 const Data = ({ data }: any) => {
-  const post = data.markdownRemark
+  const post = data.markdownRemark;
   return (
     <Layout>
       <Seo
@@ -66,10 +69,10 @@ const Data = ({ data }: any) => {
         <MarkdownContent dangerouslySetInnerHTML={{ __html: post.html }} />
       </Content>
     </Layout>
-  )
-}
+  );
+};
 
-export default Data
+export default Data;
 
 export const pageQuery = graphql`
   query ($path: String!) {
@@ -84,4 +87,4 @@ export const pageQuery = graphql`
       timeToRead
     }
   }
-`
+`;
